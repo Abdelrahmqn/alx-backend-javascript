783b2eee80935d1c8d93a898ff262b066c50ec1d
Abderlahman <boda8099@gmail.com>
2024-07-30 17:38:57 -0700

    fix

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,1 +1,6 @@
-#!/usr/bin/node
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
