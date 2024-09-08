10fafb79fb4c8c6f7f3aeb476807a9ba11d211ec
Abdelrahman <boda8099@gmail.com>
2024-09-08 19:54:45 +0300

    test

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+function handleResponseFromAPI(promise) {
+    return promise
+    .then(() => {
+        console.log('Got a response from the API');
+        return {
+            status: 200,
+            body: 'Success'
+        };
+    })
+    .catch(() => {
+        return new Error();
+    });
+}
+export default handleResponseFromAPI;
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from "./utils.js";
+function handleProfileSignup() {
+    const photoPromise = new uploadPhoto();
+    const userPromise = new createUser();
+  
+    return Promise.all([photoPromise, userPromise])
+      .then(([photoData, userData]) => {
+        console.log(photoData.body, userData.firstName, userData.lastName);
+      })
+      .catch(() => {
+        console.log('Signup system offline');
+      });
+  }
+  
+  export default handleProfileSignup;
