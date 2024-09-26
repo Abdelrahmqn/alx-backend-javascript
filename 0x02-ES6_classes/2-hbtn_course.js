0ca4e26fd032afc8b55b950b47ee501b9c358538
Abdelrahman <boda8099@gmail.com>
2024-09-26 03:41:59 +0300

    1, 2, 3

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,20 @@
 export default class HolbertonCourse {
+  /**
+   * Constructor: Ensures the object is created with valid initial data.
+Setters: Protect the properties from invalid updates after the object is created.
+   *
+   */
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!(students instanceof Array) || !students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -32,10 +47,7 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (!(value instanceof Array)) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    if (!value.every((student) => typeof student === 'string')) {
+    if (!(value instanceof Array) || !value.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
