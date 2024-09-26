1e0f0df630d86fd0fd2b53e56997581a4b39e034
Abdelrahman <boda8099@gmail.com>
2024-09-26 16:31:22 +0300

    air

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,11 +1,5 @@
 export default class Airport {
   constructor(name, code) {
-    if (typeof name !== 'string') {
-      throw new TypeError('name must be a string');
-    }
-    if (typeof code !== 'string') {
-      throw new TypeError('code must be a string');
-    }
     this._name = name;
     this._code = code;
   }
@@ -33,6 +27,6 @@ export default class Airport {
   }
 
   toString() {
-    return this._code;
+    return `[object ${this._code}]`;
   }
 }
