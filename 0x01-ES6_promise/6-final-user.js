e7c44f0eb7cb728c9f41ea005d603f716e697d01
Abdelrahman <boda8099@gmail.com>
2024-09-29 01:25:24 +0300

    i promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,11 @@
-import { signUpUser } from './4-user-promise';
-import { uploadphoto } from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadphoto(fileName)])
-    .then((result) => (
-      result.map((mapping) => ({
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((mapping) => ({
         status: mapping.status,
         value: mapping.status === 'fulfilled' ? mapping.value : String(mapping.reason),
       }))
