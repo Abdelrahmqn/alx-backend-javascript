055a2e3f7ab9960342b09b1bd8726dc65691185a
Abdelrahman <boda8099@gmail.com>
2024-09-29 00:28:26 +0300

    5

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,12 @@
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        const succ = true;
-        if (succ) {
-            resolve('okay');
-        } else {
-            reject('no');
-        }
-    });
+  return new Promise((resolve, reject) => {
+    const succ = true;
+    if (succ) {
+      resolve('okay');
+    } else {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject('no');
+    }
+  });
 }
 export default getResponseFromAPI;
